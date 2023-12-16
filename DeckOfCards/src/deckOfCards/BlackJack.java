cb9857a915a1c4eb85357bdac85c3bf63e8ac1f4
Salvarez99 <Stephenalvarez3@gmail.com>
2023-12-15 21:55:42 -0500

    .

--- DeckOfCards/src/deckOfCards/BlackJack.java
+++ DeckOfCards/src/deckOfCards/BlackJack.java
@@ -1,12 +1,14 @@
 package deckOfCards;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlackJack{
 
     //Can make use of a hashmap to keep track of player scores
     private ArrayList<Player> players;
-    private ArrayList<Integer> playerScores;
+    private HashMap<Player, Integer> playerScores;
     private Deck deck;
     private int PLAYERCOUNT = 2;
     private int VALUE_LIMIT = 21;
@@ -14,7 +16,7 @@ public class BlackJack{
     public BlackJack() throws InvalidException{
         this.deck = new Deck("std");
         this.players = new ArrayList<>();
-        this.playerScores = new ArrayList<>();
+        this.playerScores = new HashMap<>();
 
         for (int i = 0; i < this.PLAYERCOUNT; i++) {
             this.players.add(new Player());
@@ -32,7 +34,8 @@ public class BlackJack{
     public BlackJack(int numOfPlayers) throws InvalidException{
         this.deck = new Deck("std");
         this.players = new ArrayList<>();
-        this.playerScores = new ArrayList<>();
+        this.playerScores = new HashMap<>();
+
         this.PLAYERCOUNT = numOfPlayers + 1;
 
         for (int i = 0; i < this.PLAYERCOUNT; i++) {
@@ -50,16 +53,31 @@ public class BlackJack{
 
         for (int index = 0; index < this.players.size(); index++) {
             if(index == this.players.size() - 1){
-
+                this.dealerLogic(this.players.get(this.PLAYERCOUNT - 1));
             }else{
                 System.out.println("P" + (index + 1) + ": ");
                 players.get(index).getInUseDeck().printDeck();
             }
         }
+
+        //While there are still players playing
+
+        //end loop
+
+        //Check who won
+
     }
 
-    private void dealerLogic(){
+    private void dealerLogic(Player player){
+        if(this.canHit(player)){
+            Card drawnCard = this.deck.drawFromTop();
+            player.getInUseDeck().addCardToDeck(drawnCard);
+            player.getInUseDeck().printDeck();
+            System.out.println("[]");
 
+        }else{
+            System.out.println("Could not hit, needs to wait to show cards now");
+        }
     }
 
     private boolean canHit(Player player){
@@ -71,7 +89,7 @@ public class BlackJack{
             sum += card.getValue();
 
             //this will add mutiple scores, more than player amount
-            this.playerScores.add(sum);
+            this.playerScores.put(player, sum);
         }
 
         if(sum < this.VALUE_LIMIT){
@@ -80,5 +98,14 @@ public class BlackJack{
         return false;
     }
 
+    public void checkWinner(){
 
+        //need to find all max values that are less than value limit and put into a list
+        //then identify the keys associated to those values
+        //if there is one key in the list, then there is one winner
+        //if there is more than one key in the list, then it is a tie (no one wins or loses)
+        for (Map.Entry<Player, Integer> playerScores : this.playerScores.entrySet()) {
+            this.playerScores.    
+        }
+    }
 }
--- DeckOfCards/src/deckOfCards/Driver.java
+++ DeckOfCards/src/deckOfCards/Driver.java
@@ -5,5 +5,9 @@ public class Driver {
 	public static void main(String[] args) throws InterruptedException, InvalidException {
 		WarGame game = new WarGame();
 		game.run();
+
+
+
+		
 	}
 }
